9d535388516d7916377b4813e3f4a0857672fa65
HapiFive <1143910315@qq.com>
2025-05-26 22:51:07 +0800

    完成udp通信

--- UdpCommunication/MainPage.xaml.cs
+++ UdpCommunication/MainPage.xaml.cs
@@ -1,24 +1,86 @@
-﻿namespace UdpCommunication
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using UdpCommunication.Utils;
+
+namespace UdpCommunication
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        readonly Socket socket;
+        const int DEFAULT_PORT = 8720;
+        const int FIND_PORT_ATTEMPTS = 10;
+        readonly int port;
+        readonly System.Timers.Timer timer;
 
         public MainPage()
         {
             InitializeComponent();
+            socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+            {
+                EnableBroadcast = true
+            };
+
+            port = DEFAULT_PORT;
+            int i = 0;
+            for (; i < FIND_PORT_ATTEMPTS; i++)
+            {
+                try
+                {
+                    socket.Bind(new IPEndPoint(IPAddress.Any, port));
+                    break;
+                }
+                catch (Exception)
+                {
+                    port++;
+                }
+            }
+
+            if (i == FIND_PORT_ATTEMPTS)
+            {
+                throw new Exception("Failed to claim a socket port");
+            }
+            
+            LocalIpPortEditor.Text = socket.LocalEndPoint?.ToString() ?? "发生错误";
+
+            
+            // 创建计时器，间隔 100ms
+            timer = new(100);
+            // 绑定事件
+            timer.Elapsed += (e, arg) =>
+            {
+                while (socket.Available > 0)
+                {
+                    try
+                    {
+                        var buffer = new byte[socket.Available];
+                        EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Loopback, 8720);
+                        socket.ReceiveFrom(buffer, ref remoteEndPoint);
+                        string msg = Encoding.UTF8.GetString(buffer);
+                        Application.Current?.Dispatcher.Dispatch(() =>
+                        {
+                            RemotIpPortEditor.Text = remoteEndPoint.ToString();
+                            ReceiveMessageEditor.Text += $"{remoteEndPoint}:{msg}\n";
+                        });
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                timer.Start();
+            };
+            timer.AutoReset = false; // 是否重复触发
+            timer.Start();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private async void SendMessageBtn_Clicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await socket.SendToAsync(Encoding.UTF8.GetBytes(MessageEditor.Text), IpPortParser.ParseIpPort(RemotIpPortEditor.Text));
         }
     }
 }
---
+++ UdpCommunication/Utils/IpPortParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UdpCommunication.Utils
+{
+    internal class IpPortParser
+    {
+        private static readonly Regex IpPortRegex = new Regex(
+        @"^(?:\[(?<ip>.+)\]|(?<ip>[^:]+)):(?<port>\d+)$",
+        RegexOptions.Compiled | RegexOptions.ExplicitCapture
+    );
+
+        public static IPAddress ParseIp(string input)
+        {
+            var match = IpPortRegex.Match(input);
+            if (!match.Success)
+            {
+                throw new FormatException("无效的 IP:端口 格式");
+            }
+
+            var ip = match.Groups["ip"].Value;
+
+            // 验证 IP 格式
+            if (!IPAddress.TryParse(ip, out IPAddress? ipAddress))
+            {
+                throw new FormatException("无效的 IP 地址格式");
+            }
+
+            return ipAddress;
+        }
+
+        public static IPEndPoint ParseIpPort(string input)
+        {
+            var match = IpPortRegex.Match(input);
+            if (!match.Success)
+            {
+                throw new FormatException("无效的 IP:端口 格式");
+            }
+
+            var ip = match.Groups["ip"].Value;
+            if (!int.TryParse(match.Groups["port"].Value, out int port))
+            {
+                port = 9720;
+            }
+            // 验证 IP 格式
+            if (!IPAddress.TryParse(ip, out IPAddress? ipAddress))
+            {
+                throw new FormatException("无效的 IP 地址格式");
+            }
+
+            return new IPEndPoint(ipAddress, port);
+        }
+    }
+}
