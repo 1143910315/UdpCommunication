fc1d848e30fd009181a4abccbc7ac1da44d81290
HapiFive <1143910315@qq.com>
2025-05-29 19:04:04 +0800

    格式化代码

--- UdpCommunication/MainPage.xaml.cs
+++ UdpCommunication/MainPage.xaml.cs
@@ -8,10 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using UdpCommunication.Utils;
 
-namespace UdpCommunication
-{
-    public partial class MainPage : ContentPage
-    {
+namespace UdpCommunication {
+    public partial class MainPage : ContentPage {
         readonly Socket socket;
         const int DEFAULT_PORT = 8720;
         const int FIND_PORT_ATTEMPTS = 10;
@@ -20,130 +18,94 @@ namespace UdpCommunication
         IPEndPoint? lastEndPoint = null;
         bool running = true;
 
-        public MainPage()
-        {
+        public MainPage() {
             InitializeComponent();
-            socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
-            {
+            socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp) {
                 EnableBroadcast = true
             };
 
             port = DEFAULT_PORT;
             int i = 0;
-            for (; i < FIND_PORT_ATTEMPTS; i++)
-            {
-                try
-                {
+            for (; i < FIND_PORT_ATTEMPTS; i++) {
+                try {
                     socket.Bind(new IPEndPoint(IPAddress.Any, port));
                     break;
-                }
-                catch (Exception)
-                {
+                } catch (Exception) {
                     port++;
                 }
             }
 
-            if (i == FIND_PORT_ATTEMPTS)
-            {
+            if (i == FIND_PORT_ATTEMPTS) {
                 throw new Exception("Failed to claim a socket port");
             }
 
             LocalIpPortEditor.Text = socket.LocalEndPoint?.ToString() ?? "发生错误";
 
-            new Thread(() =>
-            {
-                try
-                {
+            new Thread(() => {
+                try {
                     var buffer = new byte[1024];
-                    while (running)
-                    {
-                        if (socket.Poll(Timeout.InfiniteTimeSpan, SelectMode.SelectRead))
-                        {
-                            while (socket.Available > buffer.Length)
-                            {
+                    while (running) {
+                        if (socket.Poll(Timeout.InfiniteTimeSpan, SelectMode.SelectRead)) {
+                            while (socket.Available > buffer.Length) {
                                 buffer = new byte[socket.Available];
                             }
                             EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Loopback, port);
                             int readBytesLength = socket.ReceiveFrom(buffer, ref remoteEndPoint);
-                            if (AutoSendCheckbox.IsChecked)
-                            {
-                                if (remoteEndPoint is IPEndPoint endPoint)
-                                {
-                                    if (endPoint.Port == autoSendPort)
-                                    {
-                                        if (lastEndPoint != null)
-                                        {
+                            if (AutoSendCheckbox.IsChecked) {
+                                if (remoteEndPoint is IPEndPoint endPoint) {
+                                    if (endPoint.Port == autoSendPort) {
+                                        if (lastEndPoint != null) {
                                             socket.SendTo(buffer, readBytesLength, SocketFlags.None, lastEndPoint);
-                                        }
-                                        else
-                                        {
-                                            Application.Current?.Dispatcher.Dispatch(() =>
-                                            {
+                                        } else {
+                                            Application.Current?.Dispatcher.Dispatch(() => {
                                                 ReceiveMessageEditor.Text += $"丢弃来自{autoSendPort}端口的数据，因为还没有发送到这个端口的数据\n";
                                             });
                                         }
-                                    }
-                                    else
-                                    {
-                                        if (lastEndPoint?.Port != endPoint.Port)
-                                        {
+                                    } else {
+                                        if (lastEndPoint?.Port != endPoint.Port) {
                                             lastEndPoint = endPoint;
-                                            Application.Current?.Dispatcher.Dispatch(() =>
-                                            {
+                                            Application.Current?.Dispatcher.Dispatch(() => {
                                                 ReceiveMessageEditor.Text += $"转发来自{autoSendPort}端口的数据到{endPoint}\n";
                                             });
                                         }
                                         socket.SendTo(buffer, readBytesLength, SocketFlags.None, new IPEndPoint(IPAddress.Loopback, autoSendPort));
                                     }
                                 }
-                            }
-                            else
-                            {
+                            } else {
                                 string msg = Encoding.UTF8.GetString(buffer, 0, readBytesLength);
-                                Application.Current?.Dispatcher.Dispatch(() =>
-                                {
+                                Application.Current?.Dispatcher.Dispatch(() => {
                                     RemotIpPortEditor.Text = remoteEndPoint.ToString();
                                     ReceiveMessageEditor.Text += $"{remoteEndPoint}:{msg}\n";
                                 });
                             }
                         }
                     }
-                }
-                catch (Exception)
-                {
+                } catch (Exception) {
                 }
             }).Start();
         }
 
-        private async void SendMessageBtn_Clicked(object sender, EventArgs e)
-        {
+        private async void SendMessageBtn_Clicked(object sender, EventArgs e) {
             IPEndPoint? iPEndPoint = IpPortParser.GetEndPointByName(RemotIpPortEditor.Text);
-            if (iPEndPoint != null)
-            {
+            if (iPEndPoint != null) {
                 await socket.SendToAsync(Encoding.UTF8.GetBytes(MessageEditor.Text), iPEndPoint);
-            }
-            else
-            {
+            } else {
                 ReceiveMessageEditor.Text += $"尝试发送到 {RemotIpPortEditor.Text} 失败，请检查输入是否正确。\n";
             }
         }
 
-        private void AutoSendTapGesture_Tapped(object sender, TappedEventArgs e)
-        {
+        private void AutoSendTapGesture_Tapped(object sender, TappedEventArgs e) {
             AutoSendCheckbox.IsChecked = !AutoSendCheckbox.IsChecked;
         }
 
-        private void AutoSendEditor_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (ushort.TryParse(AutoSendEditor.Text, out ushort newPort))
-            {
+        private void AutoSendEditor_TextChanged(object sender, TextChangedEventArgs e) {
+            if (ushort.TryParse(AutoSendEditor.Text, out ushort newPort)) {
                 autoSendPort = newPort;
                 AutoSendLabel.Text = $"自动转发数据到指定端口{newPort}";
             }
         }
 
-        private void ContentPage_Disappearing(object sender, EventArgs e)
-        {
+        private void ContentPage_Disappearing(object sender, EventArgs e) {
             running = false;
             socket.Close();
         }
--- UdpCommunication/Utils/IpPortParser.cs
+++ UdpCommunication/Utils/IpPortParser.cs
@@ -7,60 +7,49 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace UdpCommunication.Utils
-{
-    internal class IpPortParser
-    {
+namespace UdpCommunication.Utils {
+    internal class IpPortParser {
         private static readonly Regex IpPortRegex = new Regex(
         @"^(?:\[(?<ip>.+)\]|(?<ip>[^:]+)):(?<port>\d+)$",
         RegexOptions.Compiled | RegexOptions.ExplicitCapture
     );
 
-        public static IPAddress ParseIp(string input)
-        {
+        public static IPAddress ParseIp(string input) {
             var match = IpPortRegex.Match(input);
-            if (!match.Success)
-            {
+            if (!match.Success) {
                 throw new FormatException("无效的 IP:端口 格式");
             }
 
             var ip = match.Groups["ip"].Value;
 
             // 验证 IP 格式
-            if (!IPAddress.TryParse(ip, out IPAddress? ipAddress))
-            {
+            if (!IPAddress.TryParse(ip, out IPAddress? ipAddress)) {
                 throw new FormatException("无效的 IP 地址格式");
             }
 
             return ipAddress;
         }
 
-        public static IPEndPoint ParseIpPort(string input)
-        {
+        public static IPEndPoint ParseIpPort(string input) {
             var match = IpPortRegex.Match(input);
-            if (!match.Success)
-            {
+            if (!match.Success) {
                 throw new FormatException("无效的 IP:端口 格式");
             }
 
             var ip = match.Groups["ip"].Value;
-            if (!int.TryParse(match.Groups["port"].Value, out int port))
-            {
+            if (!int.TryParse(match.Groups["port"].Value, out int port)) {
                 port = 9720;
             }
             // 验证 IP 格式
-            if (!IPAddress.TryParse(ip, out IPAddress? ipAddress))
-            {
+            if (!IPAddress.TryParse(ip, out IPAddress? ipAddress)) {
                 throw new FormatException("无效的 IP 地址格式");
             }
 
             return new IPEndPoint(ipAddress, port);
         }
-        public static IPEndPoint? GetEndPointByName(string name)
-        {
+        public static IPEndPoint? GetEndPointByName(string name) {
             string[] parts = name.Split(':');
-            if (parts.Length != 2)
-            {
+            if (parts.Length != 2) {
                 // RainMeadow.Debug("Invalid IP format without colon: " + name);
                 parts = new string[2];
                 parts[0] = name;
@@ -68,32 +57,27 @@ namespace UdpCommunication.Utils
             }
 
             IPAddress? address = null;
-            try
-            {
+            try {
                 address = IPAddress.Parse(parts[0]);
-            }
-            catch (FormatException)
-            {
-                try
-                {
+            } catch (FormatException) {
+                try {
                     address = Dns.GetHostEntry(parts[0])
                         .AddressList
                         .Where(x => x.AddressFamily == AddressFamily.InterNetwork)
                         .FirstOrDefault();
-                }
-                catch (SocketException)
-                {
-                    if (address == null) return null;
+                } catch (SocketException) {
+                    if (address == null)
+                        return null;
                 }
             }
 
-            if (!ushort.TryParse(parts[1], out ushort port))
-            {
+            if (!ushort.TryParse(parts[1], out ushort port)) {
                 //RainMeadow.Debug("Invalid port format: " + parts[1]);
                 return null;
             }
 
-            if (address == null) return null;
+            if (address == null)
+                return null;
             return new IPEndPoint(address, port);
         }
     }
